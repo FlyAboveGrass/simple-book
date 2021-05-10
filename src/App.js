7b0a255bca54f69b4ce7961bcd8d434f32e44262
yangjiajian <287023075@qq.com>
2021-05-10 10:11:14 +0800

    使用 react-router-dom 实现路由管理

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import Header from "./common/header";
 import { Provider } from 'react-redux'
 import { Component } from "react";
 import store from './store'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Home from './pages/home'
+import Login from './pages/login'
 
 class App extends Component {
   render() {
     return (
       <Provider store={ store }>
-        <Header></Header>
+        <BrowserRouter>
+        { /*  一定要有一个div 作为顶层容器 */}
+          <div>
+            <Header></Header>
+            <Route path="/" exact component={ Home }></Route>
+            <Route path="/login" exact component={ Login }></Route>
+          </div>
+        </BrowserRouter>
       </Provider>
     )
   }
